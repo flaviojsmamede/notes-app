ccbf35e4647a0527db69828ad14a0a62be168ba8
Flavio Mamede <flaviojsmamede@gmail.com>
2023-09-29 17:32:11 +0100

    filter tags logic

--- src/NoteList.tsx
+++ src/NoteList.tsx
@@ -2,15 +2,28 @@ import { useState } from "react";
 import { Button, Col, Form, FormGroup, Row, Stack } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import ReactSelect from "react-select"
-import { Tag } from "./App";
+import { Note, Tag } from "./App";
 
 type NoteListProps = {
   availableTags: Tag[]
+  notes: Note[]
 }
 
-export function NoteList( availableTags: NoteListProps ) {
+export function NoteList({ availableTags, notes }: NoteListProps ) {
   const [selectedTags, setSelectedTags] = useState<Tag[]>([])
   const [title, setTitle] = useState("")
+
+  const filteredNotes = useMemo(() => {
+    return notes.filter(notes => {
+      return (
+        (title === "" ||
+        notes.title.toLowerCase().includes(title.toLowerCase())) &&
+        (selectedTags.every(tag =>
+          notes.tags.some(noteTag => noteTag.id === tag.id)
+        ))
+      )
+    })
+  }, [notes, selectedTags, title])
 
   return <>
     <Row className="align-items-center mb-4">
@@ -30,7 +43,7 @@ export function NoteList( availableTags: NoteListProps ) {
       <Row className="mb-4">
         <Col>
           <Form.Group controlId="title">
-            <Form.Label>Title</Form.Label>
+            <Form.Label>Filter by title</Form.Label>
             <Form.Control
               type="text"
               value={title}
@@ -39,7 +52,7 @@ export function NoteList( availableTags: NoteListProps ) {
         </Col>
         <Col>
           <FormGroup controlId="tags">
-            <Form.Label>Tags</Form.Label>
+            <Form.Label>Filter by tags</Form.Label>
             <ReactSelect
               value={selectedTags.map(tag => {
                   return { label: tag.label, value: tag.id}
@@ -55,8 +68,15 @@ export function NoteList( availableTags: NoteListProps ) {
               isMulti
             />
           </FormGroup>
-    </Col>
+        </Col>
       </Row>
     </Form>
+    <Row xs={1} sm={2} lg={3} xl={4} className="g-3">
+      {filteredNotes.map(note => (
+        <Col key={note.id}>
+          <NoteCard />
+        </Col>
+      ))}
+    </Row>
   </>
 }
